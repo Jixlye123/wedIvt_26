db5b62f00710db0082adcdd87555cf62c0fbad68
Jinuka123 <sethjinuka@gmail.com>
2026-06-01 17:39:15 +0530

    dsfdsf

--- wedding-invite/src/app/layout.tsx
+++ wedding-invite/src/app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     siteName: "Nipuni & Samith Wedding",
     images: [
       {
-        url: "/Gemini_Generated_Image_s201ots201ots201.png",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "Nipuni & Samith Wedding",
@@ -57,7 +57,7 @@ export const metadata: Metadata = {
     title: "Nipuni & Samith's Wedding Invitation 💍",
     description:
       "Join us as we celebrate the marriage of Nipuni Dilthara & Samith Lakshan – June 26, 2026",
-    images: ["/Gemini_Generated_Image_s201ots201ots201.png"],
+    images: ["/og-image.png"],
   },
   manifest: "/manifest.json",
   themeColor: "#c9a96e",
